40c9c8a03d5165be674efa1a93be92e007340d61
Ernest Roca Vivó <khenblack@gmail.com>
2018-12-31 12:32:09 +0100

    Add OAuth google

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamsCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
+
+// BrowserRouter -> localhost:3000/pagetwo
+// HashRouter -> localhost:3000/#/pagetwo
+// MemoryRouter -> localhost:3000
 
 const App = () => {
-  return <div>App</div>
+
+  return (
+    <div className='ui container'>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path='/' exact component={StreamList} />
+          <Route path='/streams/new' component={StreamsCreate} />
+          <Route path='/streams/edit' component={StreamEdit} />
+          <Route path='/streams/delete' component={StreamDelete} />
+          <Route path='/streams/show' component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
